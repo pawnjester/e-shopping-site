8ecbe8c58983808bf2571479d319c08bc948fd71
niinyarko <niinyarko1@gmail.com>
2017-11-18 16:39:29 +0000

    temp changes.  # Please enter the commit message for your changes. Lines starting

--- app/src/main/java/co/loystar/loystarbusiness/activities/BirthdayOffersAndMessagingActivity.java
+++ app/src/main/java/co/loystar/loystarbusiness/activities/BirthdayOffersAndMessagingActivity.java
@@ -1,13 +1,15 @@
 package co.loystar.loystarbusiness.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import co.loystar.loystarbusiness.R;
 
 public class BirthdayOffersAndMessagingActivity extends AppCompatActivity {
-
+    private ViewPager mViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,11 @@ public class BirthdayOffersAndMessagingActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        TabLayout tabLayout = findViewById(R.id.activity_birthday_offers_and_messaging_tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Birthday Offer"));
+        tabLayout.addTab(tabLayout.newTab().setText("Birthday Message Text"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
 
 }
---
+++ app/src/main/java/co/loystar/loystarbusiness/fragments/BirthdayOffersFragment.java
@@ -0,0 +1,27 @@
+package co.loystar.loystarbusiness.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.loystar.loystarbusiness.R;
+
+public class BirthdayOffersFragment extends Fragment {
+
+    public BirthdayOffersFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_birthday_offers, container, false);
+    }
+
+}
