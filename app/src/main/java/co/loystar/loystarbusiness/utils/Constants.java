9c5930e6df37fd869663db061cfe6d83b5b1ce06
niinyarko <niinyarko1@gmail.com>
2017-12-19 10:43:01 +0000

    fcm works.

--- app/src/main/java/co/loystar/loystarbusiness/utils/Constants.java
+++ app/src/main/java/co/loystar/loystarbusiness/utils/Constants.java
@@ -36,4 +36,6 @@ public class Constants {
     public static final String FRAGMENT_EVENT_ID = "FRAGMENT_EVENT_ID";
     public static final String ACTIVITY_INITIATOR = "ACTIVITY_INITIATOR";
     public static final String FIREBASE_REGISTRATION_TOKEN = "FIREBASE_REGISTRATION_TOKEN";
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
 }
--- app/src/main/java/co/loystar/loystarbusiness/utils/fcm/MyFirebaseInstanceIDService.java
+++ app/src/main/java/co/loystar/loystarbusiness/utils/fcm/MyFirebaseInstanceIDService.java
@@ -1,27 +1,13 @@
 package co.loystar.loystarbusiness.utils.fcm;
 
-import android.accounts.AccountManager;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import co.loystar.loystarbusiness.R;
-import co.loystar.loystarbusiness.auth.SessionManager;
-import co.loystar.loystarbusiness.auth.api.ApiClient;
-import co.loystar.loystarbusiness.auth.sync.AccountGeneral;
 import co.loystar.loystarbusiness.utils.Constants;
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * Created by ordgen on 12/18/17.
@@ -35,59 +21,26 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
      * the previous token had been compromised. Note that this is called when the InstanceID token
      * is initially generated so this is where you would retrieve the token.
      */
-    // [START refresh_token]
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-        sendRegistrationToServer(refreshedToken);
+        storeRegIdInPref(refreshedToken);
     }
-    // [END refresh_token]
 
     /**
-     * Persist token to third-party servers.
-     *
-     * Modify this method to associate the user's FCM InstanceID token with any server-side account
-     * maintained by your application.
+     * Persist token to shared preferences.
      *
      * @param token The new token.
      */
-    private void sendRegistrationToServer(String token) {
-        ApiClient apiClient = new ApiClient(getApplicationContext());
-        JSONObject jsonObjectData = new JSONObject();
-        try {
-            jsonObjectData.put("token", token);
-            JSONObject requestData = new JSONObject();
-            requestData.put("data", jsonObjectData);
+    private void storeRegIdInPref(String token) {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(Constants.FIREBASE_REGISTRATION_TOKEN, token);
+        editor.apply();
 
-            RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), requestData.toString());
-            apiClient.getLoystarApi(false).setFirebaseRegistrationToken(requestBody).enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
-                    if (response.isSuccessful()) {
-                        SharedPreferences pref = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), 0);
-                        SharedPreferences.Editor editor = pref.edit();
-                        editor.putString(Constants.FIREBASE_REGISTRATION_TOKEN, token);
-                        editor.apply();
-                    } else if (response.code() == 401) {
-                        SessionManager sessionManager = new SessionManager(getApplicationContext());
-                        AccountManager accountManager = AccountManager.get(getApplicationContext());
-                        accountManager.invalidateAuthToken(AccountGeneral.ACCOUNT_TYPE, sessionManager.getAccessToken());
-                    }
-                }
-
-                @Override
-                public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
-
-                }
-            });
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        SendFirebaseRegistrationToken sendFirebaseRegistrationToken = new SendFirebaseRegistrationToken(getApplicationContext());
+        sendFirebaseRegistrationToken.sendRegistrationToServer();
     }
 }
