9c18ab8c363904444a78cdfe683a4885d8b57ed2
niinyarko <niinyarko1@gmail.com>
2018-02-12 03:35:11 +0000

    sales history activity works.

--- app/src/main/java/co/loystar/loystarbusiness/activities/SalesHistoryActivity.java
+++ app/src/main/java/co/loystar/loystarbusiness/activities/SalesHistoryActivity.java
@@ -1,22 +1,73 @@
 package co.loystar.loystarbusiness.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.content.res.AppCompatResources;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.darwindeveloper.onecalendar.clases.Day;
+import com.darwindeveloper.onecalendar.views.OneCalendarView;
 
 import java.util.Date;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import co.loystar.loystarbusiness.R;
-import co.loystar.loystarbusiness.fragments.CardSalesHistoryFragment;
-import co.loystar.loystarbusiness.fragments.CashSalesHistoryFragment;
+import co.loystar.loystarbusiness.auth.SessionManager;
+import co.loystar.loystarbusiness.models.DatabaseManager;
+import co.loystar.loystarbusiness.models.entities.LoyaltyProgramEntity;
+import co.loystar.loystarbusiness.models.entities.MerchantEntity;
+import co.loystar.loystarbusiness.models.entities.SaleEntity;
 import co.loystar.loystarbusiness.utils.Constants;
+import co.loystar.loystarbusiness.utils.Foreground;
+import co.loystar.loystarbusiness.utils.ui.buttons.BrandButtonNormal;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.requery.Persistable;
+import io.requery.reactivex.ReactiveEntityStore;
+import timber.log.Timber;
 
 public class SalesHistoryActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CHOOSE_PROGRAM = 110;
+
+    @BindView(R.id.saleDateCalendarSelect)
+    OneCalendarView calendarView;
+
+    @BindView(R.id.noSalesView)
+    View noSalesView;
+
+    @BindView(R.id.calendarBloc)
+    View calendarBloc;
+
+    @BindView(R.id.stateImage)
+    ImageView stateImage;
+
+    @BindView(R.id.stateIntroText)
+    TextView stateIntroText;
+
+    @BindView(R.id.stateDescriptionText)
+    TextView stateDescriptionText;
+
+    @BindView(R.id.stateActionBtn)
+    BrandButtonNormal stateActionBtn;
+
+    private Context mContext;
+    private ReactiveEntityStore<Persistable> mDataStore;
+    private SessionManager mSessionManager;
+    private MerchantEntity merchantEntity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +80,12 @@ public class SalesHistoryActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        mContext = this;
+        ButterKnife.bind(this);
+        mDataStore = DatabaseManager.getDataStore(this);
+        mSessionManager = new SessionManager(this);
+        merchantEntity = mDataStore.findByKey(MerchantEntity.class, mSessionManager.getMerchantId()).blockingGet();
+
         Date preselectedSaleDate = null;
         if (getIntent().hasExtra(Constants.SALE_DATE)) {
             preselectedSaleDate = (Date) getIntent().getSerializableExtra(Constants.SALE_DATE);
@@ -39,46 +96,139 @@ public class SalesHistoryActivity extends AppCompatActivity {
 
         }
 
-        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        calendarView.setOneCalendarClickListener(new OneCalendarView.OneCalendarClickListener() {
+            @Override
+            public void dateOnClick(Day day, int position) {
+                Timber.e("DATE: %s", day);
+            }
 
-        // Set up the ViewPager with the sections adapter.
-        ViewPager mViewPager = findViewById(R.id.activity_sales_history_vp);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+            @Override
+            public void dateOnLongClick(Day day, int position) {
 
-        TabLayout tabLayout = findViewById(R.id.tabs);
+            }
+        });
 
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+        calendarView.setOnCalendarChangeListener(new OneCalendarView.OnCalendarChangeListener() {
+            @Override
+            public void prevMonth() {
 
+            }
+
+            @Override
+            public void nextMonth() {
+
+            }
+        });
+
+        stateActionBtn.setOnClickListener(view -> startSale());
+
+        mDataStore.count(SaleEntity.class).get().single().subscribe(integer -> {
+            if (integer == 0) {
+                showNoSalesView();
+            }
+        });
     }
 
     /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
+     * Shows the no sales view and hides the calendar view.
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showNoSalesView() {
+        int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-        SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+        noSalesView.setVisibility(View.VISIBLE);
+        stateImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.ic_firstsale));
+        stateIntroText.setText(getString(R.string.hello_text, mSessionManager.getFirstName()));
+        stateDescriptionText.setText(getString(R.string.start_sale_empty_state));
+        stateActionBtn.setText(getString(R.string.start_sale_btn_label));
 
-        @Override
-        public Fragment getItem(int position) {
-            Fragment fragment = null;
-            switch (position) {
-                case 0:
-                    fragment = new CashSalesHistoryFragment();
-                    break;
-                case 1:
-                    fragment = new CardSalesHistoryFragment();
-                    break;
+        noSalesView.animate()
+            .setDuration(shortAnimTime)
+            .alpha(1)
+            .setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                calendarBloc.setVisibility(View.GONE);
             }
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
+        });
     }
+
+    private void startSale() {
+        mDataStore.count(LoyaltyProgramEntity.class)
+            .get()
+            .single()
+            .toObservable()
+            .subscribe(new Observer<Integer>() {
+                @Override
+                public void onSubscribe(Disposable d) {
+
+                }
+
+                @Override
+                public void onNext(Integer integer) {
+                    if (integer == 0) {
+                        if (Foreground.get().isForeground()) {
+                            new AlertDialog.Builder(mContext)
+                                .setTitle("No Loyalty Program Found!")
+                                .setMessage("To record a sale, you would have to start a loyalty program.")
+                                .setPositiveButton(mContext.getString(R.string.start_loyalty_program_btn_label), (dialog, which) -> {
+                                    dialog.dismiss();
+                                    startLoyaltyProgram();
+                                })
+                                .setNegativeButton(android.R.string.cancel, (dialog, which) -> dialog.dismiss()).show();
+                        }
+                    } else if (integer == 1) {
+                        LoyaltyProgramEntity loyaltyProgramEntity = merchantEntity.getLoyaltyPrograms().get(0);
+                        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+                        boolean isPosTurnedOn = sharedPreferences.getBoolean(getString(R.string.pref_turn_on_pos_key), false);
+                        if (isPosTurnedOn) {
+                            startSaleWithPos();
+                        } else {
+                            startSaleWithoutPos(loyaltyProgramEntity.getId());
+                        }
+                    } else {
+                        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+                        boolean isPosTurnedOn = sharedPreferences.getBoolean(getString(R.string.pref_turn_on_pos_key), false);
+                        if (isPosTurnedOn) {
+                            startSaleWithPos();
+                        } else {
+                            chooseProgram();
+                        }
+                    }
+                }
+
+                @Override
+                public void onError(Throwable e) {
+
+                }
+
+                @Override
+                public void onComplete() {
+
+                }
+            });
+    }
+
+    private void chooseProgram() {
+        Intent intent = new Intent(this, ChooseProgramActivity.class);
+        startActivityForResult(intent, REQUEST_CHOOSE_PROGRAM);
+    }
+
+    private void startSaleWithPos() {
+        Intent intent = new Intent(this, SaleWithPosActivity.class);
+        startActivity(intent);
+    }
+
+    private void startSaleWithoutPos(int programId) {
+        Intent intent = new Intent(this, SaleWithoutPosActivity.class);
+        intent.putExtra(Constants.LOYALTY_PROGRAM_ID, programId);
+        startActivity(intent);
+    }
+
+    private void startLoyaltyProgram() {
+        Intent intent = new Intent(mContext, LoyaltyProgramListActivity.class);
+        intent.putExtra(Constants.CREATE_LOYALTY_PROGRAM, true);
+        startActivity(intent);
+    }
+
 }
