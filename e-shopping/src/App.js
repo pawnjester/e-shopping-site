4a345b73fcb3fca79246d5abec4cc0e006f0b0fa
Emmanuel Okonji <emmanuel_andela@medable.com>
2020-02-12 16:30:11 +0100

    update-menu-item

--- e-shopping/src/App.js
+++ e-shopping/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
+
+const TopicsPage =() => (
+  <div>
+    <h1>
+      Topic Detail
+    </h1>
+  </div>
+)
 function App() {
   return (
-    <div >
-      <Homepage />
+    <div>
+        <Route exact path="/" component= {Homepage} />
+        <Route exact path="/shop/hats" component= {TopicsPage} />
     </div>
   );
 }
