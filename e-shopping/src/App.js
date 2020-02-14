1541595c95bcd55fe4cb659fe4ded1c8c44ccef9
Emmanuel Okonji <emmanuel_andela@medable.com>
2020-02-15 00:36:36 +0100

    implemeneted firebase utils including to store auth users

--- e-shopping/src/App.js
+++ e-shopping/src/App.js
@@ -6,7 +6,7 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from  './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 
 
 
@@ -23,12 +23,22 @@ class App extends React.Component {
   unSubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState ({
-        currentUser: user
-      })
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+          console.log(this.state);
+        })
+
+      }
+      this.setState({ currentUser: userAuth })
     })
   }
 
--- e-shopping/src/firebase/firebase.utils.js
+++ e-shopping/src/firebase/firebase.utils.js
@@ -13,10 +13,40 @@ const config = {
   measurementId: "G-L4BG00DGRT"
 };
 
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  console.log("y>>>>", userAuth);
+
+  const snapshot = await userRef.get();
+
+  if(!snapshot.exists) {
+    const {displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try{
+
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('cerror creating ', error.message);
+    }
+  }
+
+  return userRef;
+
+}
+
 firebase.initializeApp(config);
+export const firestore = firebase.firestore();
 
 export const auth = firebase.auth();
-export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({
